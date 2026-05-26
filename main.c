01637981c8c2b4ac47c33ca25b154db450bde736
Rustibirin <rustibirin@gmail.com>
2026-05-26 22:21:42 +0200

    Adding FtPrintf

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rumartin <rumartin@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/26 21:55:21 by rumartin          #+#    #+#             */
-/*   Updated: 2026/05/26 22:01:18 by rumartin         ###   ########.fr       */
+/*   Updated: 2026/05/26 22:16:55 by rumartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 
 int	main(int argc, char **argv)
 {
+	if (argv[1][0] == '-' && argv[1][1] == '-')
+	{
+		ft_printf("El argumento introducido es es: %s", argv[1]);
+	}
+	else
+	{
+		ft_printf("No hay argumentos validos");
+	}
 	return (0);
 }
