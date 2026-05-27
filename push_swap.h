74aa7e955e9224c39b077d9c6d8a740239513cb6
Francisco Ramirez Prieto <framirez@c1r8s4.42malaga.com>
2026-05-27 16:15:18 +0200

    estructura definida en header

--- push_swap.h
+++ push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rumartin <rumartin@student.42malaga.com>   +#+  +:+       +#+        */
+/*   By: framirez <framirez@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/26 21:58:20 by rumartin          #+#    #+#             */
-/*   Updated: 2026/05/26 22:01:00 by rumartin         ###   ########.fr       */
+/*   Updated: 2026/05/27 16:13:28 by framirez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,14 @@
 # define PUSH_SWAP_H
 
 // Librerias
-# include <unistd.h>
 # include <stdlib.h>
+# include <unistd.h>
+
+typedef struct stack
+{
+	int				*num;
+	struct stack	*next;
+}					node;
 
 // Prototipos
 #endif
