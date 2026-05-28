3b420253d8124967a6d4c5838e645654ee9185d6
Francisco Ramirez Prieto <framirez@c2r3s2.42malaga.com>
2026-05-28 20:01:16 +0200

    operaciones el push está fatal

--- push.c
+++ push.c
@@ -6,10 +6,35 @@
 /*   By: framirez <framirez@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/28 18:03:47 by framirez          #+#    #+#             */
-/*   Updated: 2026/05/28 18:08:27 by framirez         ###   ########.fr       */
+/*   Updated: 2026/05/28 19:52:55 by framirez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void push(t_data data)
+void	push(t_node **stack_1, t_node **stack_2, int size_1, int size_2)
+{
+	t_node	*aux_1;
+	t_node	*aux_2;
+	t_node	*aux_aux;
+
+	if (stack_2 == NULL || *stack_2 == NULL)
+		return ;
+	else
+	{
+		aux_1 = *stack_1;          // guardo el primer nodo
+		aux_2 = *stack_2;          // guardo el segundo nodo
+		aux_1->prev->next = aux_2; // control de extremos
+		aux_2->prev->next = aux_1; // control de extremos
+		aux_1->next->prev = aux_2; // control de extremos
+		aux_2->next->prev = aux_1; // control de extremos
+		aux_aux = aux_2->prev;     // empieza el bloque de los prev
+		aux_2->prev = aux_1->prev;
+		aux_1->prev = aux_aux;
+		aux_aux = aux_2->next; // empieza el bloque de los next
+		aux_2->next = aux_1->next;
+		aux_1->next = aux_aux;
+		*stack_1 = aux_2;
+		*stack_2 = aux_1;
+	}
+}
