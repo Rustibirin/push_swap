fb71cc7ad67f30cee37cd13bccd4237c98eb515d
Rustibirin <rustibirin@gmail.com>
2026-05-31 14:01:02 +0200

    Preparada funcion para algoritmos

--- main.c
+++ main.c
@@ -6,12 +6,28 @@
 /*   By: rumartin <rumartin@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/26 21:55:21 by rumartin          #+#    #+#             */
-/*   Updated: 2026/05/31 13:38:54 by rumartin         ###   ########.fr       */
+/*   Updated: 2026/05/31 13:59:30 by rumartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int	ft_algorithms(t_data *data)
+{
+	if(data->strategy == SIMPLE)
+		if (ft_simple())
+			return (1);
+	else if(data->strategy == MEDIUM)
+		if (ft_medium())
+			return (1);
+	else if(data->strategy == COMPLEX)
+		if (ft_complex())
+			return (1);
+	else
+		if (ft_adaptive())
+			return (1);
+	return (0);
+}
 int	ft_sorted(t_node *stack)
 {
 	t_node	*act_node;
@@ -30,10 +46,17 @@ int	ft_sorted(t_node *stack)
 
 int	main(int argc, char **argv)
 {
-	if (argc > 1)
-	{
-		ft_checker(argv);
-		printf("Hola");
-	}
-	return (0);
+	t_data	*data;
+
+	if (argc < 2)
+		return (0);
+	data = ft_calloc(1, sizeof(t_data));
+	if (!data)
+		return (1);
+	if (ft_args_checker(argv, data))
+		return (write(2, "Error\n", 6), ft_free_stack(&data->stack_a),
+			free(data), 1);
+	if (ft_sorted)
+		ft_algorithms(data);
+	return (ft_free_stack(&data->stack_a), free(data), 0);
 }
