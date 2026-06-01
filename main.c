ccf7d18bd657cd1fb9ae7b523976ec856c025f95
Rustibirin <rustibirin@gmail.com>
2026-06-01 11:24:45 +0200

    Se adapta la funcion del subject para calcular el desorden

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rumartin <rumartin@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/26 21:55:21 by rumartin          #+#    #+#             */
-/*   Updated: 2026/05/31 14:05:37 by rumartin         ###   ########.fr       */
+/*   Updated: 2026/06/01 11:23:52 by rumartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,53 @@
 int	ft_algorithms(t_data *data)
 {
 	if(data->strategy == SIMPLE)
-		if (ft_simple())
+	{
+		if (ft_simple(data))
 			return (1);
+	}
 	else if(data->strategy == MEDIUM)
-		if (ft_medium())
+	{
+		if (ft_medium(data))
 			return (1);
+	}
 	else if(data->strategy == COMPLEX)
-		if (ft_complex())
+	{
+		if (ft_complex(data))
 			return (1);
+	}
 	else
-		if (ft_adaptive())
+	{
+		if (ft_adaptive(data))
 			return (1);
+	}
 	return (0);
+}
+
+double	ft_compute_disorder(t_node *stack)
+{
+	t_node	*node_i;
+	t_node	*node_j;
+	long	mistakes;
+	long	total_pairs;
+
+	if (!stack || stack->next == stack)
+		return (0.0);
+	mistakes = 0;
+	total_pairs = 0;
+	node_i = stack;
+	while (node_i->next != stack)
+	{
+		node_j = node_i->next;
+		while (node_j != stack)
+		{
+			total_pairs++;
+			if (node_i->number > node_j->number)
+				mistakes++;
+			node_j = node_j->next;
+		}
+		node_i = node_i->next;
+	}
+	return ((double)mistakes / total_pairs);
 }
 
 int	ft_sorted(t_node *stack)
@@ -57,7 +92,10 @@ int	main(int argc, char **argv)
 	if (ft_args_checker(argv, data))
 		return (write(2, "Error\n", 6), ft_free_stack(&data->stack_a),
 			free(data), 1);
-	if (ft_sorted)
+	if (ft_sorted(data->stack_a))
+	{
+		ft_compute_disorder(data->stack_a);
 		ft_algorithms(data);
+	}
 	return (ft_free_stack(&data->stack_a), free(data), 0);
 }
